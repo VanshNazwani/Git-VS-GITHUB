4ac9d99f1c156adb59ed11e2591cd8a89ac85034
Vansh Nazwani <nazwaniv@gmail.com>
2025-09-29 18:54:03 +0530

    new features added

--- git-crash-course/index.js
+++ git-crash-course/index.js
@@ -1,7 +1,7 @@
 const age = 24;
 const dob = "01/01/2000";
 const name = "John Doe";
-
+const message = "Hello, World!";
 
 function name() {
     log(`my name is ${name}`);    
@@ -11,4 +11,6 @@ function is18() {
     return age >= 18;
 }
 
-6
+function message(message) {
+    console.log('this is a message: ' + message);
+}
